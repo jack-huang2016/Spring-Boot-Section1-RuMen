935a8d19442f9e3b98a24058a44765501bcd767e
jack-huang2016 <2456120975@qq.com>
2018-12-05 20:24:42 +0800

    Spring Boot 入门demo

---
+++ src/main/java/org/springboot/sample/controller/HelloController.java
@@ -0,0 +1,37 @@
+package org.springboot.sample.controller;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController   //知识点记录一：@RestController注解的使用
+@RequestMapping("/hello")
+public class HelloController {
+	@RequestMapping  //知识点记录二：@RequestMapping注解的默认路径
+    public String hello() {
+        return "Hello Spring-Boot";
+    }
+
+    @RequestMapping("/info")
+    public Map<String, String> getInfo(@RequestParam String name) {
+        Map<String, String> map = new HashMap<>();
+        map.put("name", name);
+        return map;
+    }
+
+    @RequestMapping("/list")
+    public List<Map<String, String>> getList() {
+        List<Map<String, String>> list = new ArrayList<>();
+        Map<String, String> map = null;
+        for (int i = 1; i <= 5; i++) {
+            map = new HashMap<>();
+            map.put("name", "Shanhy-" + i);
+            list.add(map);
+        }
+        return list;
+    }
+}
